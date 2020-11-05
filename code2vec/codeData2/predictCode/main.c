fc73d2db43c46ad62dde945b218176b7d3fdc0ea
dongyu1997 <1452103565@qq.com>
2020-11-05 18:42:18 +0800

    添加需求

--- code2vec/codeData2/predictCode/main.c
+++ code2vec/codeData2/predictCode/main.c
@@ -1,50 +1,8 @@
 #include <stdio.h>
-#include <assert.h>
 
-int main(){
-	int i=0,y;
-	float x;
-	char a[500],b[500];
-	while((a[i]=getchar())!='\n'){
-		i++;
-	}
-	a[i]='\0';
-	x=i;
-	i=0;
-	while((b[i]=getchar())!='\n'){
-		i++;
-	}
-	b[i]='\0';
-	y=i;
-	
-	float count=0;
-	int j=0;
-	i=0;
-	
-	printf("%d\n", y);
-
-	while(a[i]!='\0'){
-		if(b[j]==a[i]){
-			count ++;
-			j++; 
-			printf("j %d\n", j);
-		}
-		if(j!=y-1 && i==x-1){
-			j++;
-			i=0;
-		}
-		assert(j < y);
-		if(j==y-1){
-			break;
-		}
-		i++;
-	}
-	
-	
-	if(count/x>=0.5){
-		printf("Yes");
-	}else{
-		printf("No");
-	}
-	return 0;
+int main()
+{
+  	int a,b,c;
+  	scanf("%d%d" ,&a,&b);
+	printf("%d\n",c);  
 }
